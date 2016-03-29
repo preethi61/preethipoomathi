0fdd9971908b81e654feb8fab2448bb03f8a0c2d
preethi61 <preethi.13bit2061@mkce.ac.in>
2016-03-29 16:57:29 +0530

    Added files via upload

---
+++ Dbconnection.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pack;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Dbconnection {
+    
+    public static Connection getConn()throws Exception{
+         Class.forName("com.mysql.jdbc.Driver");
+         Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/captcha","root","root");
+        return con;
+    
+    }
+   
+}
